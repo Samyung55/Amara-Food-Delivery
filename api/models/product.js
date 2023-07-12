88859e40823abfd604e3e05a60dc3e9bd07556ec
Sam Fred <123533544+Samyung55@users.noreply.github.com>
2023-07-12 03:35:33 -0700

    Create product.js

---
+++ api/models/product.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    Image: {
+        type: String,
+        required: true,
+    },
+    category: {
+        type: String,
+        required: true,
+    },
+    type: {
+        type: String,
+        default: "Veg"
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    rating: {
+        type: Number,
+        default: 5
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    countInStock: {
+        type: Number,
+    }
+}, {
+    timeStamps: true
+})
+
+const Product = mongoose.model('Product', productSchema)
+export default Product;
