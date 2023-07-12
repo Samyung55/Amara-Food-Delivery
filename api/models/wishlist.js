4fbd9cece75fd7e3b8d8bf13304e306331e67191
Sam Fred <123533544+Samyung55@users.noreply.github.com>
2023-07-12 03:35:46 -0700

    Create wishlist.js

---
+++ api/models/wishlist.js
@@ -0,0 +1,38 @@
+const moongoose = require("mongoose")
+
+const wishListSchema = new moongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    image:{
+        type:String,
+        required:true,
+    },
+    price:{
+        type:Number,
+        required:true,
+    },
+    rating:{
+        type:Number,
+        required:true,
+    },
+    description:{
+        type:String,
+        required:true,
+    },
+    userId:{   
+        type:moongoose.Schema.Types.ObjectId,
+        ref:'User',
+        required:true
+    },
+    product:{
+        type:moongoose.Schema.Types.ObjectId,
+        ref:'Product',
+        required:true
+    }
+    
+})
+const Wishlist= moongoose.model('Wishlist',wishListSchema)
+export default Wishlist;
