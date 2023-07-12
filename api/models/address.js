c72d371e415ff39029409c53ebef6a5c2d7c5ba5
Sam Fred <123533544+Samyung55@users.noreply.github.com>
2023-07-12 03:35:19 -0700

    Create address.js

---
+++ api/models/address.js
@@ -0,0 +1,41 @@
+const moongoose = require("mongoose")
+
+const addressSchema = new moongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    mobNo: {
+        type: Number,
+        required: true,
+    },
+    pinCode: {
+        type: Number,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true,
+    },
+    town: {
+        type: String,
+        required: true
+    },
+    state: {
+        type: String,
+        required: true
+    },
+    city: {
+        type: String,
+        required: true,
+    },
+    userId: {
+        type: String,
+        required: true,
+    }
+}, {
+    timestamps: true
+})
+
+const Address = mongoose.model('Address',addressSchema)
+export default Address;
