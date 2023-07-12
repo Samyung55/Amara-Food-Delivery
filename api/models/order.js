becca9f8d30a494d2b06dcb3ff0af18282bfed4c
Sam Fred <123533544+Samyung55@users.noreply.github.com>
2023-07-12 03:35:26 -0700

    Create order.js

---
+++ api/models/order.js
@@ -0,0 +1,147 @@
+const mongoose = require("mongoose")
+
+const orderSchema = new mongoose.Schema({
+    orderItems: [{
+        name: { 
+        type: String,
+        required: true
+    },
+    qty: {
+        type: Number,
+        required: true
+    },
+    Image: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    customItem: [{
+        name: {
+            type: String
+        },
+        price: {
+            type: Number
+        },
+        type: {
+            type: String
+        }
+    }],
+    isCustom: {
+        type: Boolean,
+        default: false,
+    },
+    product: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product', required: true
+    }
+    }],
+
+    shippingAddress: {
+        name: {
+            type: String,
+            required: true
+        },
+        mobNo: {
+            type: Number,
+            required: true,
+        },
+        pinCode: {
+            type: Number,
+            required: true
+        },
+        address: {
+            type: String,
+            required: true
+        },
+        town: {
+            type: String,
+            required: true,
+        },
+        state: {
+            type: String,
+            required: true,
+        },
+        city: {
+            type: String,
+            required: true
+        },
+    },
+
+    paymentMethod: {
+        type: String,
+        required: true
+    },
+
+    shippingPrice: {
+        type: Number,
+        required: true
+    },
+
+    itemsPrice: {
+        type: Number,
+        required: true
+    },
+
+    totalPrice: {
+        type: Number,
+        required: true
+    },
+
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User', 
+        required: true
+    },
+
+    isPaid: {
+        type: Boolean,
+        default: false
+    },
+
+    paidAt: {
+        type: Date
+    },
+
+    isDelivered: {
+        type: Boolean,
+        default: false
+    },
+
+    isPlaced: {
+        type: Boolean,
+        default: true
+    },
+
+    isShipped: {
+        type: Boolean,
+        default: false
+    },
+
+    deliveryDate: {
+        type: Date
+    },
+
+    paymentId: {
+        type: String
+    },
+
+    email: {
+        type: String,
+        required: true
+    },
+
+    userName: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+}
+)
+
+
+const Order = mongoose.model('Order', orderSchema);
+export default Order;
