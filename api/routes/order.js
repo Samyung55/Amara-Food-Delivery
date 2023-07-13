9065477bb8c309ec80d28fbdc2f087ad1a899d8b
Sam Fred <123533544+Samyung55@users.noreply.github.com>
2023-07-13 07:56:37 -0700

    Update order.js

--- api/routes/order.js
+++ api/routes/order.js
@@ -12,3 +12,5 @@ const CLIENT_SECRET='';
 const REDIRECT_URI='';
 const REFRESH_TOKEN='';
 
+const oAuth2Client = new google.auth.OAuth2(CLIENT_ID,CLIENT_SECRET,REDIRECT_URI);
+oAuth2Client.setCredentials({refresh_token:REFRESH_TOKEN});
