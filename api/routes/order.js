ae05e71d7ff866091bbe7cc82187360bee2c679f
Sam Fred <123533544+Samyung55@users.noreply.github.com>
2023-07-13 07:48:49 -0700

    Update order.js

--- api/routes/order.js
+++ api/routes/order.js
@@ -4,3 +4,11 @@ import nodemailer from 'nodemailer'
 import {google} from 'googleapis'
 import Order from "../models/order.js";
 import { emailTemplate, isAuth } from "../utlis.js";
+
+const orderRouter= express.Router()
+
+const CLIENT_ID='';
+const CLIENT_SECRET='';
+const REDIRECT_URI='';
+const REFRESH_TOKEN='';
+
