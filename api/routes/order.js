8c832d7ac3e78a80fe665db7d173e2ead6dd68c5
Sam Fred <123533544+Samyung55@users.noreply.github.com>
2023-07-13 07:39:59 -0700

    Create order.js

---
+++ api/routes/order.js
@@ -0,0 +1,6 @@
+import express from "express";
+import expressAsyncHandler from "express-async-handler";
+import nodemailer from 'nodemailer'
+import {google} from 'googleapis'
+import Order from "../models/order.js";
+import { emailTemplate, isAuth } from "../utlis.js";
