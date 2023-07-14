87d6a811c19d47e384962c42bef438900324074e
Sam Fred <123533544+Samyung55@users.noreply.github.com>
2023-07-14 15:33:12 -0700

    Update order.js

--- api/routes/order.js
+++ api/routes/order.js
@@ -14,3 +14,13 @@ const REFRESH_TOKEN='';
 
 const oAuth2Client = new google.auth.OAuth2(CLIENT_ID,CLIENT_SECRET,REDIRECT_URI);
 oAuth2Client.setCredentials({refresh_token:REFRESH_TOKEN});
+
+orderRouter.get('/',isAuth,expressAsyncHandler(async(req,res)=>{
+    const orders = await Order.find({userId:req.user._id})
+    res.send(orders)
+}))
+orderRouter.get('/admin-orders',expressAsyncHandler(async(req,res)=>{
+    const orders = await Order.find().sort({_id:-1}).limit(20)
+    res.send(orders)
+}))
+
