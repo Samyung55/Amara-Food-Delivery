114f428a54ee72a535343ad16128fb5cf58aa2fe
Sam Fred <123533544+Samyung55@users.noreply.github.com>
2023-07-14 15:38:07 -0700

    Update product.js

--- api/routes/product.js
+++ api/routes/product.js
@@ -6,3 +6,9 @@ const Wishlist = require("../models/wishlist.js");
 const { isAuth } = require("../utlis.js");
 
 const productRouter = express.Router();
+
+productRouter.get('/',expressAsyncHandler(async(req,res)=>{
+    const products = await Product.find({category:req.query.category}) //return all products
+    res.send(products)
+  }));
+  
