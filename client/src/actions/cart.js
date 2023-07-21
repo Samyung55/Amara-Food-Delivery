b6c69e46cbe33625d4d0f235b85890c0505eb752
Sam Fred <123533544+Samyung55@users.noreply.github.com>
2023-07-21 17:31:35 +0100

    Update cart.js

--- client/src/actions/cart.js
+++ client/src/actions/cart.js
@@ -4,28 +4,28 @@ import { ADD_TO_CART, DELETE_ITEM, ERROR, UPDATE_QTY } from "./types"
 export const addToCart= ( item ) => async (dispatch,getState) => {
     try {
         //  const {data} = pizza.get(`/api/pizzas/${data.id}`)
-         dispatch({type:ADD_TO_CART,payload:{
-             name:item.name,
-             price:item.price,
-             image:item.image,
-             description:item.description,
-             product:item._id,
-             customItem:item.customItem?item.customItem:[],
-             isCustom:item.isCustom?item.isCustom:false,
-             qty:1
+         dispatch({ type: ADD_TO_CART, payload:{
+             name: item.name,
+             price: item.price,
+             image: item.image,
+             description: item.description,
+             product: item._id,
+             customItem: item.customItem ? item.customItem: [],
+             isCustom: item.isCustom ? item.isCustom: false,
+             qty: 1
             }
             })
         localStorage.setItem('cartItems',JSON.stringify( getState().cart.cartItems)) 
      } catch (error) {
-         dispatch({type:ERROR,payload:'Failed to Add!'})
+         dispatch({type:ERROR, payload:'Failed to Add!'})
      }  
 }
 
-export const upadteCart =(product,qty)=>async(dispatch,getState)=>{
+export const upadteCart = ( product, qty ) => async (dispatch,getState) => {
     // console.log({...product,qty:qty})
     try {
-        dispatch({type:UPDATE_QTY,payload:{...product,qty:qty}})
-    localStorage.setItem('cartItems',JSON.stringify( getState().cart.cartItems)) 
+        dispatch({ type: UPDATE_QTY,payload: {...product, qty: qty}})
+    localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems)) 
 
     } catch (error) {
         dispatch({type:ERROR,payload:'Failed to Update!'})
