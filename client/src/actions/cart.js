c3c726c71b2ff394c4101611df4558df79096f20
Sam Fred <123533544+Samyung55@users.noreply.github.com>
2023-07-21 17:28:51 +0100

    Update cart.js

---
+++ client/src/actions/cart.js
@@ -0,0 +1,6 @@
+import pizza from "../apis/pizza";
+import { ADD_TO_CART, DELETE_ITEM, ERROR, UPDATE_QTY } from "./types"
+
+export const addToCart= ( item ) => async (dispatch,getState) => {
+   
+}
