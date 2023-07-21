0359a10babee16b937ab688b11b11ca591cdfa40
Sam Fred <123533544+Samyung55@users.noreply.github.com>
2023-07-21 11:44:17 +0100

    Update address.js

--- client/src/actions/address.js
+++ client/src/actions/address.js
@@ -1,2 +1,12 @@
 import pizza from "../apis/pizza"
 import { ADDRESS_ERROR, ADD_ADDRESS, DELETE_ADDRESS, GET_ADDRESS, SELECET_ADDRESS, UPDATE_ADDRESS } from "./types"
+
+export const addAddress=(name, mobNo, pinCode, address, town, state, city, userId)=>async dispatch=>{
+    try {
+        const { data } = await pizza.post('/api/users/address',{ name, mobNo, pinCode, address, town, state, city, userId})
+        dispatch({ type: ADD_ADDRESS, payload: data})
+    } catch (error) {
+        dispatch({type: ADDRESS_ERROR, 
+            payload:error.response && error.response.data.message ? error.response.data.message:error.message })
+    }
+}
