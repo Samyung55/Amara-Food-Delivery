bb1d16096c0d17b0318b933a49ff6f862e640b82
Sam Fred <123533544+Samyung55@users.noreply.github.com>
2023-07-22 19:30:32 +0100

    Update orders.js

--- client/src/actions/orders.js
+++ client/src/actions/orders.js
@@ -2,3 +2,8 @@ import pizza from "../apis/pizza"
 import { CREATE_ORDER, CREATE_ORDER_ERROR, CREATE_ORDER_REQUEST, DELETE_CART, ORDER_DETAIlS, ORDER_DETAILS_FAIL, ORDER_DETAILS_REQURST, PAYMENT_METHOD } from "./types"
 import { doc, updateDoc ,getDoc} from "firebase/firestore";
 import { firestore, db } from '../firebase'
+
+export const selectPayment = (payment) => async dispatch => {
+    dispatch({ type: PAYMENT_METHOD, payload: payment})
+}
+
