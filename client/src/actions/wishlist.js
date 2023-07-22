3345a08e5e275a5d8a112cd5081f9824381da38b
Sam Fred <123533544+Samyung55@users.noreply.github.com>
2023-07-22 19:59:05 +0100

    Update wishlist.js

--- client/src/actions/wishlist.js
+++ client/src/actions/wishlist.js
@@ -1,2 +1,6 @@
 import pizza from "../apis/pizza"
 import { ADD_WISHLIST_ERROR, ADD_WISHLIST_REQ, ADD_WISHLIST_SUCCESS ,DELETE_WISHLIST_ITEM,GET_WISHLIST} from "./types"
+
+export const addToWishlist =(item)=>async (dispatch,getState)=>{
+    console.log(item)
+    dispatch({type:ADD_WISHLIST_REQ,payload:item})
