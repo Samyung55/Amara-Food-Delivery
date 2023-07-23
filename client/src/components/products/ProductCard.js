e7a60b08ebf8c562b9b9f1e9da4a5e94d4602c3e
Sam Fred <123533544+Samyung55@users.noreply.github.com>
2023-07-23 20:57:40 +0100

    Update ProductCard.js

---
+++ client/src/components/products/ProductCard.js
@@ -0,0 +1,9 @@
+import React, { useEffect, useState } from 'react'
+import { FaStar , FaStarHalfAlt, FaRegStar } from 'react-icons/fa';
+import { AiOutlineHeart, AiFillHeart } from 'react-icons/ai';
+import { IoMdAdd } from 'react-icons/io';
+import { useDispatch, useSelector } from 'react-redux';
+import { addToCart } from '../../actions/cart';
+import { addToWishlist ,deleteItemFromWishlist,getWishlist} from '../../actions/wishlist';
+import { useLocation } from 'react-router-dom';
+import Spinner from '../Spinner';
