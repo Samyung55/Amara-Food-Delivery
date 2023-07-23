e23728bb378d2fe1620068f8990a5ce90ff97ba9
Sam Fred <123533544+Samyung55@users.noreply.github.com>
2023-07-23 20:52:00 +0100

    Update MainArea.js

---
+++ client/src/components/MainArea.js
@@ -0,0 +1,8 @@
+import React, { useState } from 'react'
+import '../styles/mainarea.css'
+import Header from './Header'
+import pizzaboy from '../assests/pizzaboy.png'
+import pizzaSilce from '../assests/pizzaSilce.png'
+import Product from './products/Product'
+import { useSelector } from 'react-redux'
+
