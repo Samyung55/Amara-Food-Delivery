546d5ffec2aabed0ae6c03fa0a27bda5fb877114
Sam Fred <123533544+Samyung55@users.noreply.github.com>
2023-07-23 20:40:20 +0100

    Update Header.js

---
+++ client/src/components/Header.js
@@ -0,0 +1,8 @@
+import React, { useState } from 'react'
+import {FiSearch} from 'react-icons/fi'
+import {HiMenuAlt1} from 'react-icons/hi'
+import {Link, useNavigate} from 'react-router-dom'
+import {useDispatch, useSelector} from 'react-redux'
+import { searchProducts, showSideBar } from '../actions'
+import Spinner from './Spinner'
+
