293434b564038417b074b839bef9bd8dc31e8c75
Sam Fred <123533544+Samyung55@users.noreply.github.com>
2023-07-23 21:22:55 +0100

    Update Product.js

--- client/src/components/products/Product.js
+++ client/src/components/products/Product.js
@@ -4,3 +4,18 @@ import {useDispatch, useSelector } from 'react-redux';
 import SkeletonArticle from '../../skeleton/SkeletonArticle';
 import { fetchPizzas } from '../../actions';
 import Message from '../Message';
+
+const Product = ({ category }) => {
+    const dispatch = useDispatch()
+    const allPizzas = useSelector(state => state.allPizza)
+    const { loading, error, data } = allPizzas 
+
+    const [show, setShow] = useState(error)
+
+      useEffect(() => {
+        if(category) {
+          dispatch(fetchPizzas(category))
+        }
+      },[category])
+
+}
