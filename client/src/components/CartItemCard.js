8a9c06cb204532488d096bbf90b38749f249ce4f
Sam Fred <samyung05@gmail.com>
2023-07-24 16:39:28 -0400

    Update CartItemCard.js

--- client/src/components/CartItemCard.js
+++ client/src/components/CartItemCard.js
@@ -2,4 +2,10 @@ import React,{useState} from 'react'
 import {MdDelete} from 'react-icons/md'
 import {IoMdAdd,IoMdRemove} from 'react-icons/io'
 import { useDispatch } from 'react-redux'
-import { deleteItem, upadteCart } from '../actions/cart'
+import { deleteItem, updateCart } from '../actions/cart'
+
+const CartItem = ({item}) => {
+    const [qty, setQty] = useState(item.qty)
+    const dispatch = useDispatch()
+    
+}
