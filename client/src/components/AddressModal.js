136ebb3a52ee9b8f553f09087df9f2b837ff964b
Sam Fred <samyung05@gmail.com>
2023-07-24 16:21:50 -0400

    Update AddressModal.js

--- client/src/components/AddressModal.js
+++ client/src/components/AddressModal.js
@@ -4,5 +4,13 @@ import { addAddress, updateAddress } from '../actions/address';
 // import axios from 'axios'
 
 const AddressModal = ({show, setShow, addressToEdit}) => {
+    const [pinCode, setPinCode] = useState('');
+    const [name, setName] = useState('')
+    const [mobno, setMobNo] = useState('')
+    const [address, setAddress] = useState('')
+    const [state,setState]=useState('')
+    const [town,setTown]=useState('')
+    const [city,setCity]=useState('')
+
     
 }
