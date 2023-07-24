305f4ee77a2bdb282b56414a2e87453321965545
Sam Fred <samyung05@gmail.com>
2023-07-24 16:17:52 -0400

    Update AddressModal.js

--- client/src/components/AddressModal.js
+++ client/src/components/AddressModal.js
@@ -3,3 +3,6 @@ import {useDispatch, useSelector} from 'react-redux'
 import { addAddress, updateAddress } from '../actions/address';
 // import axios from 'axios'
 
+const AddressModal = ({show, setShow, addressToEdit}) => {
+    
+}
