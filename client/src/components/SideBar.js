07169e53b0e1169b61a49fe996a3b5562dca561e
Sam Fred <123533544+Samyung55@users.noreply.github.com>
2023-07-26 22:45:27 +0100

    Update SideBar.js

---
+++ client/src/components/SideBar.js
@@ -0,0 +1,11 @@
+import React ,{useState}from 'react'
+import '../styles/sidebar.css'
+import {FaHome,FaBoxOpen,FaPizzaSlice,FaTimes} from 'react-icons/fa'
+import {BsHeartHalf,BsGear} from 'react-icons/bs'
+import {CgFileDocument} from 'react-icons/cg'
+import { Link,useLocation } from 'react-router-dom'
+import {FiLogOut} from 'react-icons/fi'
+import { useDispatch, useSelector } from 'react-redux'
+import { logout } from '../actions/auth'
+import { showSideBar } from '../actions'
+
