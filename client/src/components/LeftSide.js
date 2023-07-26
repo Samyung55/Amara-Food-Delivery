b11e33cef9c67838799d3f5bb42afbcfaf81c16a
Sam Fred <123533544+Samyung55@users.noreply.github.com>
2023-07-26 22:32:55 +0100

    Update LeftSide.js

---
+++ client/src/components/LeftSide.js
@@ -0,0 +1,9 @@
+import React, { useState } from 'react'
+import '../styles/leftside.css'
+import {BsCart3,BsFillArrowRightSquareFill} from 'react-icons/bs'
+import {IoNotificationsOutline} from 'react-icons/io5'
+import {Link} from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import CartItemCard from './CartItemCard'
+import delivery from '../assests/delivery.png'
+
