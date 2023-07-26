a1201481b73f876995ddb6bdd8b2fd53c0eb8df9
Sam Fred <123533544+Samyung55@users.noreply.github.com>
2023-07-26 22:47:24 +0100

    Update AddProduct.js

--- client/src/pages/AddProduct.js
+++ client/src/pages/AddProduct.js
@@ -7,3 +7,22 @@ import pizza from '../apis/pizza'
 import { doc, setDoc } from "firebase/firestore"; 
 import { db, firestore } from '../firebase'
 
+const AddProduct = () => {
+    const [img,setImg]=useState("")
+    const [name,setName]=useState("")
+    const [des,setDes]=useState("")
+    const [price,setPrice]=useState()
+    const [show,setShow]=useState(false)
+    const [category,setCategory]=useState("")
+    const [fileName,setFileName]=useState("Add Image")
+    const [loading,setLoading]=useState(false)
+    const handleSubmit=async(e)=>{
+        e.preventDefault()
+        setLoading(true)
+      const {data}=await pizza.post("/api/products/add-product",{name,description:des,price:parseInt(price),image:img,category}) 
+      await setDoc(doc(firestore,db.pizzas,name),{
+        name,
+        inStockItem:7,
+        outOfStock:false,
+        img
+      });
