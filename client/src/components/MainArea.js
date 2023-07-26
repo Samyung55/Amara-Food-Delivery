a0706ddc59ff63a60c35e35fd4f0a1fa53591e58
Sam Fred <123533544+Samyung55@users.noreply.github.com>
2023-07-26 22:36:00 +0100

    Update MainArea.js

--- client/src/components/MainArea.js
+++ client/src/components/MainArea.js
@@ -12,12 +12,18 @@ const MainArea = () => {
 
     return (
         <div className='mainarea'>
-            <Header />
-            <div className='banner'>
-                <div className='img'>
-                    <img src={pizzaboy} alt='' />
+            <Header/>
+            <div className="banner">
+                <div className="img">
+                    <img src={pizzaboy} alt="" />
                 </div>
-            </div>
-        </div>
-    )
-}
+                <div className="text">
+                <h2>Hello {user?.user?.name}</h2>
+                <p>Get Free delivery on <span>500 Rs.</span>  and above</p>
+                <button>Order Now!</button>
+                <img className='full circle' src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/da/Orange_circle_100%25.svg/768px-Orange_circle_100%25.svg.png" alt="" />
+                <img className='small circle' src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/da/Orange_circle_100%25.svg/768px-Orange_circle_100%25.svg.png" alt="" />
+                <img className='smaller circle' src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/da/Orange_circle_100%25.svg/768px-Orange_circle_100%25.svg.png" alt="" />
+               
+                <img className='half circle' src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/da/Orange_circle_100%25.svg/768px-Orange_circle_100%25.svg.png" alt="" />
+               
