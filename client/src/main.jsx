be76cae2342a0aa6a288a7e73c182680344b8f45
Sam Fred <123533544+Samyung55@users.noreply.github.com>
2023-07-27 13:14:39 +0100

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import {createStore,applyMiddleware,compose} from 'redux'
+import thunk from 'redux-thunk'
+import App from './App'
+import reducers from './reducers'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+const initialState = {
+  cart:{
+    cartItems:localStorage.getItem('cartItems')?JSON.parse(localStorage.getItem('cartItems')):[]
+
+  },
+  user:{
+    user:localStorage.getItem('User')?JSON.parse(localStorage.getItem('User')):null
+  }
+}
+const composeEnhancers=window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__||compose
+const store = createStore(reducers,initialState,composeEnhancers(applyMiddleware(thunk)))
+ReactDOM.render(
+    <Provider store={store}>
+      <App/>
+    </Provider>,
+    document.getElementById('root')
 )
